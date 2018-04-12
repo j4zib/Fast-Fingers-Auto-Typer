eaad3205854ae8ad4723d34422c102cc14c58830
tokyojack <tokyojack2001@yahoo.com>
2018-04-11 20:39:24 -0700

    Refractor code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from selenium import webdriver;
 
-driver = webdriver.Chrome('./drivers/chromedriver.exe')
+def start_typing(driver):
+    inputField = driver.find_element_by_id("inputfield");
 
-driver.set_page_load_timeout(10)
-driver.get("https://10fastfingers.com/typing-test/english")
+    for i in range(345):
+        try:
+            text = driver.find_element_by_xpath("//span[@wordnr='" + str(i) + "']").text
+            inputField.send_keys(text)
+            inputField.send_keys(" ")
+        except:
+            print("Done")
+            break
 
-for i in range(338):
-    text = driver.find_element_by_xpath("//span[@wordnr='" + str(i) + "']").text + " "
-    for letter in text:
-        driver.find_element_by_id("inputfield").send_keys(letter)
+if __name__ == '__main__':
+    driver = webdriver.Chrome('./drivers/chromedriver.exe')
+    driver.set_page_load_timeout(10)
+    driver.get("https://10fastfingers.com/typing-test/english")
+
+    start_typing(driver)
